35830d3d3389717b2480f20a8c52b4346806660e
Japneet Rajput <japneetrajput2804@gmail.com>
2022-01-11 02:28:55 +0530

    Tweaks

--- Bookstore/core/models.py
+++ Bookstore/core/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 from django.contrib.auth.models import User,AbstractUser
 
-class User(AbstractUser()):
-    email = models.EmailField(unique=true)
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
